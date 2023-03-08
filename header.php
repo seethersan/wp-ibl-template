e149bff016d908c931147f7c473bf838d0dd1e5b
Juan Carlos Espinoza Zurita <carlos_jcez@hotmail.com>
2023-03-08 16:18:33 -0500

    first commit

---
+++ header.php
@@ -0,0 +1,263 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package ibl
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+<div class="fixed">
+	<!-- <div class="top_toolbar hmb">
+		<div class="bar_row">
+			<div class="bar_text">We have developed a 100% mobile studio video production solution to help you through this challenging time</div>
+			<a href="
+			https://ibleducation.com					/content" class="bar_btn w-button">Learn More</a>
+			<div class="close_bar">
+				<a href="#" data-w-id="14896e95-21aa-ccf4-c471-8f0ef5cadedc" class="close_ba">+</a>
+			</div>
+		</div>
+	</div> -->
+
+
+	<div data-collapse="medium" data-animation="default" data-duration="400" role="banner" class="navbar w-nav">
+		<div class="navigation-container w-container">
+			<div class="navigation-left">
+				<a href="https://ibleducation.com" aria-current="page" class="brand current w-nav-brand w--current">
+													<img src="https://ibleducation.com/wp-content/themes/ibl-web-ibleducation-theme/img/logo_v2.png" width="217" alt="" class="image-7">
+
+					
+				</a>
+			</div>
+			<nav role="navigation" class="nav-menu w-nav-menu">
+				<div class="navigation-right">
+					<div data-hover="1" data-delay="0" class="menu-drop w-dropdown">
+						<div data-w-id="9826fc05-3381-3bf4-02d2-f3dad16a92e9" class="nav-link-3 w-dropdown-toggle" id="w-dropdown-toggle-0" aria-controls="w-dropdown-list-0" aria-haspopup="menu" aria-expanded="false" role="button" tabindex="0">
+							<div class="nav-link-3 i w-icon-dropdown-toggle"></div>
+							<div class="nav-link-3">About</div>
+						</div>
+						<nav class="db_list w-dropdown-list" id="w-dropdown-list-0" aria-labelledby="w-dropdown-toggle-0">
+							<a href="https://ibleducation.com/about" class="db_l w-dropdown-link" tabindex="0">About IBL</a>
+							<a href="https://ibleducation.com/clients" class="db_l w-dropdown-link" tabindex="0">Testimonials</a>
+							<a href="https://ibleducation.com/nvidia" class="db_l w-dropdown-link" tabindex="0">NVIDIA Deep Learning <br>Institute’s Case Study</a>
+							<a href="https://ibleducation.com/global-knowledge" class="db_l w-dropdown-link" tabindex="0">Global Knowledge’s Case Study</a>
+							<a href="https://ibleducation.com/the-george-washington-university" class="db_l w-dropdown-link" tabindex="0">The George Washington <br>University’s Case Study</a>
+							<a href="https://ibleducation.com/modern-states" class="db_l w-dropdown-link" tabindex="0">Modern States Education’s <br>Case Study</a>
+						</nav>
+					</div>
+					<div data-hover="1" data-delay="0" class="menu-drop w-dropdown">
+						<div data-w-id="e143ac98-fc2e-dcad-4108-f7620f6d0c8c" class="nav-link-3 w-dropdown-toggle" id="w-dropdown-toggle-1" aria-controls="w-dropdown-list-1" aria-haspopup="menu" aria-expanded="false" role="button" tabindex="0">
+							<div class="nav-link-3 i w-icon-dropdown-toggle"></div>
+							<div class="nav-link-3">IBL Platform</div>
+						</div>
+						<nav class="db_list w-dropdown-list" id="w-dropdown-list-1" aria-labelledby="w-dropdown-toggle-1">
+							<a href="https://ibleducation.com/platform" class="db_l w-dropdown-link" tabindex="0">Deployment &amp; Maintenance</a>
+							<a href="https://ibleducation.com/analytics" class="db_l w-dropdown-link" tabindex="0">Deep Analytics Pipeline</a>
+							<a href="https://ibleducation.com/training" class="db_l w-dropdown-link" tabindex="0">Training on Open edX™</a>
+							<a href="https://ibleducation.com/consulting" class="db_l w-dropdown-link" tabindex="0">Consulting Services</a>
+							<a href="https://ibleducation.com/learning-communities" class="db_l w-dropdown-link" tabindex="0">Learning Communities &amp; <br>Academies</a>
+							<a href="https://ibleducation.com/custom-learning-platforms-with-credentials" class="db_l w-dropdown-link" tabindex="0">Custom Learning Platforms <br>With Credentials</a>
+							<a href="https://ibleducation.com/ui-ux" class="db_l w-dropdown-link" tabindex="0">UI / UX</a>
+							<a href="https://iblnews.org/?s=ChatGPT" target="_blank" class="db_l w-dropdown-link" tabindex="0">News on AI and ChatGPT</a>
+
+						</nav>
+					</div>
+					<div data-hover="1" data-delay="0" class="menu-drop w-dropdown">
+						<div data-w-id="475423eb-a74a-f77d-3043-09fd857f1a6f" class="nav-link-3 w-dropdown-toggle" id="w-dropdown-toggle-2" aria-controls="w-dropdown-list-2" aria-haspopup="menu" aria-expanded="false" role="button" tabindex="0">
+							<div class="nav-link-3 i w-icon-dropdown-toggle"></div>
+							<div class="nav-link-3">IBL Studios</div>
+						</div>
+						<nav class="db_list w-dropdown-list" id="w-dropdown-list-2" aria-labelledby="w-dropdown-toggle-2">
+							<a href="https://ibleducation.com/content" class="db_l w-dropdown-link" tabindex="0">Production Studios (NYC or <br>Mobile Studio)</a>
+							<a href="https://ibleducation.com/courses-developed-by-our-studio" class="db_l w-dropdown-link" tabindex="0">Courses Developed at Our <br>Studios</a>
+							<a href="https://ibleducation.com/instructional-design-services" class="db_l w-dropdown-link" tabindex="0">Instructional Design Services</a>
+							<a href="https://ibleducation.com/ibl-educations-courses-and-programs-for-business" class="db_l w-dropdown-link" tabindex="0">IBL Education for Business: <br>Courses and Programs</a>
+							<!-- <a href="https://ibleducation.com/studio-solutions" class="db_l w-dropdown-link">Studio Solutions</a> -->
+							<a href="https://ibleducation.com/iblstudios" class="db_l w-dropdown-link" tabindex="0">IBL Studios 2022</a>
+
+						</nav>
+					</div>
+					<div data-hover="1" data-delay="0" class="menu-drop w-dropdown">
+						<div data-w-id="a0fcfe74-8a40-357f-1861-750413a5212c" class="nav-link-3 w-dropdown-toggle" id="w-dropdown-toggle-3" aria-controls="w-dropdown-list-3" aria-haspopup="menu" aria-expanded="false" role="button" tabindex="0">
+							<div class="nav-link-3 i w-icon-dropdown-toggle"></div>
+							<div class="nav-link-3">Open edX</div>
+						</div>
+						<nav class="db_list w-dropdown-list" id="w-dropdown-list-3" aria-labelledby="w-dropdown-toggle-3">
+							<a href="https://iblnews.org/?s=edx" target="_blank" class="db_l w-dropdown-link" tabindex="0">News on Learning Innovation <br>&amp; Open edX</a>
+							<a href="https://iblnews.org/category/views/" target="_blank" class="db_l w-dropdown-link" tabindex="0">Views &amp; Analysis</a>
+							<a href="https://ibleducation.com/open-edx" class="db_l w-dropdown-link" tabindex="0">Commonly Asked Questions</a>
+							<a href="https://ibleducation.com/contributions-to-the-open-edx-community" class="db_l w-dropdown-link" tabindex="0">Contributions to the edX Community</a>
+						</nav>
+					</div>
+					<a href="https://ibleducation.com/aws" target="_blank" class="aws-img">
+						<img class="aws-logo grey-logo" src="https://ibleducation.com/wp-content/themes/ibl-web-ibleducation-theme/img/aws-logo-c.png" alt="">
+					</a>
+					<a href="https://ibleducation.com/contact-us" class="button-blue-2 register w-button">Contact Us</a>
+				</div>
+			</nav>
+			<div data-w-id="9fce1b4f-d010-cdaf-2b31-8bdb19735b53" class="mobile_menu_btn"><img src="https://ibleducation.com/wp-content/themes/ibl-web-ibleducation-theme/img/mob_menu.png" loading="lazy" sizes="(max-width: 767px) 12.986111640930176px, 100vw" srcset="https://ibleducation.com/wp-content/themes/ibl-web-ibleducation-theme/img/mob_menu.png 500w, https://ibleducation.com/wp-content/themes/ibl-web-ibleducation-theme/img/mob_menu.png 512w" alt="" class="open_menu">
+				<div class="dark_overlay"></div>
+			</div>
+			<div class="mob_menu_wr">
+				<div class="mob_header">
+					<div class="div-block-54">
+						<a data-w-id="aa0cca57-51bb-6681-c9d1-7a08f4ac8e0b" href="#" class="close_mobile_menu w-inline-block"><img src="https://uploads-ssl.webflow.com/5f9968a99ac2b1a9f5952d15/5f9968a99ac2b176e8952e4d_cancel%20(1).png" loading="lazy" alt="" class="image-50"></a>
+					</div>
+				</div>
+				<div class="submenu_div">
+					<div class="foundation_submenu">
+						<div data-w-id="85cec46c-033b-8932-6db6-9ba35b59d3b4" class="mob_c_courses t">
+							<div class="mob_menu_row">
+								<div class="m_c_fb">
+									<div class="res_text">About</div>
+									<img src="https://ibleducation.com/wp-content/themes/ibl-web-ibleducation-theme/img/5f29919565ec6c81b25782c3_down-arrow-1.png" width="14" sizes="100vw" srcset="https://ibleducation.com/wp-content/themes/ibl-web-ibleducation-theme/img/5f29919565ec6c81b25782c3_down-arrow-1.png 500w, https://ibleducation.com/wp-content/themes/ibl-web-ibleducation-theme/img/5f29919565ec6c81b25782c3_down-arrow-1.png 512w" alt="" class="image-36h res"><img src="https://ibleducation.com/wp-content/themes/ibl-web-ibleducation-theme/img/5f29919565ec6c81b25782c3_down-arrow-1.png" alt="" class="image-36 res">
+								</div>
+							</div>
+						</div>
+						<div class="submenu_div-2">
+							<div class="about_sub">
+								<a href="https://ibleducation.com/about" class="sub_menu_option w-inline-block">
+									<div class="menu_title">About IBL</div>
+								</a>
+								<a href="https://ibleducation.com/clients" class="sub_menu_option w-inline-block">
+									<div class="menu_title">Testimonials</div>
+								</a>
+								<a href="https://ibleducation.com/nvidia" class="sub_menu_option w-inline-block">
+									<div class="menu_title">NVIDIA Deep Learning Institute's Case Study</div>
+								</a>
+								<a href="https://ibleducation.com/global-knowledge" class="sub_menu_option w-inline-block">
+									<div class="menu_title">Global Knwoledge's Case Study</div>
+								</a>
+								<a href="https://ibleducation.com/the-george-washington-university" class="sub_menu_option w-inline-block">
+									<div class="menu_title">The George Washington University's Case Study</div>
+								</a>
+								<a href="https://ibleducation.com/modern-states" class="sub_menu_option w-inline-block">
+									<div class="menu_title">Modern States Educations's Case Study</div>
+								</a>
+							</div>
+						</div>
+					</div>
+					<div class="foundation_submenu">
+						<div data-w-id="97633de0-26d2-5eee-fc27-54cf8df2a51b" class="mob_c_courses">
+							<div class="mob_menu_row">
+								<div class="m_c_fb">
+									<div class="res_text">IBL Platform</div><img src="https://ibleducation.com/wp-content/themes/ibl-web-ibleducation-theme/img/5f29919565ec6c81b25782c3_down-arrow-1.png" width="14" sizes="100vw" srcset="https://ibleducation.com/wp-content/themes/ibl-web-ibleducation-theme/img/5f29919565ec6c81b25782c3_down-arrow-1.png 500w, https://ibleducation.com/wp-content/themes/ibl-web-ibleducation-theme/img/5f29919565ec6c81b25782c3_down-arrow-1.png 512w" alt="" class="image-36h res"><img src="https://ibleducation.com/wp-content/themes/ibl-web-ibleducation-theme/img/5f29919565ec6c81b25782c3_down-arrow-1.png" alt="" class="image-36 res">
+								</div>
+							</div>
+						</div>
+						<div class="submenu_div-2">
+							<div class="platform_sub">
+								<a href="https://ibleducation.com/platform" class="sub_menu_option w-inline-block">
+									<div class="menu_title">Deployment &amp; Maintenance</div>
+								</a>
+								<a href="https://ibleducation.com/analytics" class="sub_menu_option w-inline-block">
+									<div class="menu_title">Deep Analytics Pipeline</div>
+								</a>
+								<a href="https://ibleducation.com/training" class="sub_menu_option w-inline-block">
+									<div class="menu_title">Training on Open edX™</div>
+								</a>
+								<a href="https://ibleducation.com/consulting" class="sub_menu_option w-inline-block">
+									<div class="menu_title">Consulting Services</div>
+								</a>
+								<a href="https://ibleducation.com/learning-communities" class="sub_menu_option w-inline-block">
+									<div class="menu_title">Learning Communities &amp;&nbsp;Academies</div>
+								</a>
+								<a href="https://ibleducation.com/custom-learning-platforms-with-credentials" class="sub_menu_option w-inline-block">
+									<div class="menu_title">Custom Learning Platforms With Credentials</div>
+								</a>
+								<a href="https://ibleducation.com/ui-ux" class="sub_menu_option w-inline-block">
+									<div class="menu_title">UI / UX</div>
+								</a>
+
+								<a href="https://iblnews.org/?s=ChatGPT" target="_blank" class="sub_menu_option w-inline-block">
+									<div class="menu_title">News on AI and ChatGPT</div>
+								</a>
+
+								
+							</div>
+						</div>
+					</div>
+					<div class="foundation_submenu">
+						<div data-w-id="9d536b61-f952-5980-52cb-939861e2a99f" class="mob_c_courses">
+							<div class="mob_menu_row">
+								<div class="m_c_fb">
+									<div class="res_text">IBL Studios</div><img src=" https://ibleducation.com/wp-content/themes/ibl-web-ibleducation-theme/img/5f29919565ec6c81b25782c3_down-arrow-1.png" width="14" sizes="100vw" srcset=" https://ibleducation.com/wp-content/themes/ibl-web-ibleducation-theme/img/5f29919565ec6c81b25782c3_down-arrow-1.png 500w,  https://ibleducation.com/wp-content/themes/ibl-web-ibleducation-theme/img/5f29919565ec6c81b25782c3_down-arrow-1.png 512w" alt="" class="image-36h res">
+									<img src=" https://ibleducation.com/wp-content/themes/ibl-web-ibleducation-theme/img/5f29919565ec6c81b25782c3_down-arrow-1.png" alt="" class="image-36 res">
+								</div>
+							</div>
+						</div>
+						<div class="submenu_div-2">
+							<div class="inst_sub">
+								<a href="https://ibleducation.com/content" class="sub_menu_option w-inline-block">
+									<div class="menu_title">Production Studios (NYC or Mobile Studio)</div>
+								</a>
+								<a href="https://ibleducation.com/courses-developed-by-our-studio" class="sub_menu_option w-inline-block">
+									<div class="menu_title">Courses Developed at Our Studios</div>
+								</a>
+								<a href="https://ibleducation.com/instructional-design-services" class="sub_menu_option w-inline-block">
+									<div class="menu_title">Instructional Design Services</div>
+								</a>
+								<a href="https://ibleducation.com/ibl-educations-courses-and-programs-for-business" class="sub_menu_option w-inline-block">
+									<div class="menu_title">IBL Education for Business: Courses and Programs</div>
+								</a>
+								<!-- <a href="https://ibleducation.com/studio-solutions" class="sub_menu_option w-inline-block">
+									<div class="menu_title">Studio Solutions</div>
+								</a> -->
+								<a href="https://ibleducation.com/iblstudios" class="sub_menu_option w-inline-block">
+									<div class="menu_title">IBL Studios Homepage</div>
+								</a>
+							</div>
+						</div>
+					</div>
+					<div class="foundation_submenu">
+						<div data-w-id="ee69ba2c-24e3-e2c4-083e-0102ef12b1cc" class="mob_c_courses">
+							<div class="mob_menu_row">
+								<div class="m_c_fb">
+									<div class="res_text">Open edX</div><img src="https://ibleducation.com/wp-content/themes/ibl-web-ibleducation-theme/img/5f29919565ec6c81b25782c3_down-arrow-1.png" width="14" sizes="100vw" srcset="https://ibleducation.com/wp-content/themes/ibl-web-ibleducation-theme/img/5f29919565ec6c81b25782c3_down-arrow-1.png 500w, https://ibleducation.com/wp-content/themes/ibl-web-ibleducation-theme/img/5f29919565ec6c81b25782c3_down-arrow-1.png 512w" alt="" class="image-36h res"><img src=" https://ibleducation.com/wp-content/themes/ibl-web-ibleducation-theme/img/5f29919565ec6c81b25782c3_down-arrow-1.png" alt="" class="image-36 res">
+								</div>
+							</div>
+						</div>
+						<div class="submenu_div-2">
+							<div class="open_sub">
+								<a href="https://iblnews.org/?s=edx" target="_blank" class="sub_menu_option w-inline-block">
+									<div class="menu_title">News on Learning Innovation &amp; Open edX</div>
+								</a>
+								<a href="https://iblnews.org/category/views/" target="_blank" class="sub_menu_option w-inline-block">
+									<div class="menu_title">Views &amp;&nbsp;Analysis</div>
+								</a>
+								<a href="https://ibleducation.com/open-edx" class="sub_menu_option w-inline-block">
+									<div class="menu_title">Commonly Asked Questions</div>
+								</a>
+								<a href="https://ibleducation.com/contributions-to-the-open-edx-community" class="sub_menu_option w-inline-block">
+									<div class="menu_title">Contributions to the edX Community</div>
+								</a>
+							</div>
+						</div>
+					</div>
+
+					<a href="https://ibleducation.com/aws" target="_blank" class="aws-img mobile-aws">
+						<img class="aws-logo grey-logo" src="https://ibleducation.com/wp-content/themes/ibl-web-ibleducation-theme/img/aws-logo-c.png" alt="">
+					</a>
+
+					<a href="https://ibleducation.com/contact-us" class="button-blue-2 register contact w-button">Contact Us</a>
+				</div>
+			</div>
+		</div>
+	<div class="w-nav-overlay" data-wf-ignore="" id="w-nav-overlay-0"></div></div>
+</div>
